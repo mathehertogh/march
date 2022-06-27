8dd4e69a6b7273bf2088a7c377b6ac3b32b9c742
mathe <m.c.hertogh@gmail.com>
2022-06-27 17:54:16 +0200

    adder initial

---
+++ src/adder.v
@@ -0,0 +1,17 @@
+module adder(	input a,
+		input b,
+		input carry_in,
+		output sum,
+		output carry_out);
+
+	wire m, ab, ac, bc;
+
+	xor xor1(m, a, b);
+	xor xor2(sum, m, carry_in);
+
+	and and1(ab, a, b);
+	and and2(ac, a, carry_in);
+	and and3(bc, b, carry_in);
+	or or1(carry_out, ab, ac, bc);
+
+endmodule
